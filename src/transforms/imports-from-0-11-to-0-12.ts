e2d1289a2991b397b9f5fde386d541f3dcdb99ca
Martin Sosic <sosic.martin@gmail.com>
2024-02-05 21:05:59 +0100

    Some code improvements, tidied up rules, fixed some wrong extensions in them.

--- src/transforms/imports-from-0-11-to-0-12.ts
+++ src/transforms/imports-from-0-11-to-0-12.ts
@@ -7,18 +7,25 @@ export const parser = "tsx";
 // if it matches any of the old 0.11 imports. If it does, we remove the matching
 // specifiers/names from the import declaration and add a new import declaration with
 // the new 0.12 import path and the matching specifiers.
-export default function transformer(fileInfo: FileInfo, api: API, options: Options) {
+export default function transformer(fileInfo: FileInfo, api: API, options: Options): string {
+  // Skip rewriting any non JS/TS files.
   if (!([".js", ".ts", ".jsx", ".tsx"].some((ext) => fileInfo.path.endsWith(ext)))) {
     return fileInfo.source;
   }
 
   const j = api.jscodeshift;
-  const root = j(fileInfo.source);
+  const astRoot = j(fileInfo.source);
 
-  // Key is the new import path, value is an array of specifiers to import from that path.
+  // These are the new imports that we will add to the file.
+  // We collect them here as we analyze the file, then at the end we write them to file.
+  // We use the function below, `addImportToNewImports`, to add them to this map.
+  // Key is the new import path, value is an array of specifiers (names) to import from that path.
   let newImports: Map<string, ImportSpecifier[]> = new Map();
 
-  function addImportToNewImports(source: string, specifier: ImportSpecifier) {
+  // Adds given import (path + name) to `newImports` map.
+  // @param source - new import path.
+  // @param specifier - new import specifier (name).
+  function addImportToNewImports(source: string, specifier: ImportSpecifier): void {
     const existingSpecifiers = newImports.get(source);
     if (existingSpecifiers) {
       existingSpecifiers.push(specifier);
@@ -27,93 +34,30 @@ export default function transformer(fileInfo: FileInfo, api: API, options: Optio
     }
   }
 
+  // Import mappings are defined at the bottom of the file: they define how old imports
+  // map to new imports, how are they to be rewritten.
+  // For each import mapping, we check if any of the imports in the file (in its AST) match it,
+  // and if they do, we remove these old imports from file (its AST) and add new versions of
+  // them to `newImports` map.
   for (const importMapping of importMappings) {
-    // TODO:
-    // What about special cases? Like user def? Or paths with variable parts?
-    // Cases:
-    //
-    //  1. A module name is user defined. Imported name is default.
-    //       Example: `import <myAction_> from '@wasp/actions/<myAction>';`
-    //           into `import { <myAction> as <myAction_> } from 'wasp/client/operations';`
-    //     In this case we need to detect modules that have the prefix we know about,
-    //     and then extract the name of the module and use it as the new import name.
-    //     Where: actions (hooks), queries (hooks).
-    //
-    //  2. A module name is fixed, import name is user defined.
-    //       Example: `import { type <MyAction> } from '@wasp/actions/types';`
-    //           into `import { type <MyAction> } from 'wasp/server/operations';`
-    //     In this case we don't really know the names, we know only the module name,
-    //     meaning we need to assume all the names in there are user defined.
-    //     Where: actions (types), queries (types), apis, entities.
-    //
-    //  3. A module name is user defined, imported name is fixed, and in new import
-    //     we use the old module name as new import name.
-    //       Example: `import { type UpdateAction } from '@wasp/crud/<MyCrud>';`
-    //           into `import { type <MyCrud> } from 'wasp/server/crud';`
-    //     Similar like case (1), but imported name is fixed, not default,
-    //     and also CRUD replaces stuff with just one import which makes it harder.
-    //     Actually we should probably not do this automatically and just point them to do it
-    //     manually, crud is anyway experimentally feature and it is likely very few people
-    //     are using it.
-    //     Where: crud.
-    //
-    //  4. A module name is user defined. Imported name is not default and is the same as module name.
-    //       Example: `import { <myJob> } from '@wasp/jobs/<myJob>';`
-    //           into `import { <myJob> } from 'wasp/server/jobs';`
-    //     Similar like case (1).
-    //
-    //  New import will always have fixed module name, and user defined import name (which is not default).
-    //
-    // Idea: if path is regex, we treat it in special way: it can have a capturing group. If it has one, we use it
-    //   as the name for a new import, if new import is `isUserDef`.
-    //   Also, if old name is `*`, then it matches all specifiers, and we use the same new name.
-    //
-    // - If path is regex, we execute it, and we obtain the capturing groups and pass them on.
-    //   We make user defined paths regexes with one capturing group.
-    // - If old name is '*', it matches any name, and new is also the same (and new name also has to be '*', or null or something.).
-    //   Actually, this '*' -> this is when old name is userDef, but path is not regex. That means it can be anything.
-    // - If new name is user defined, then we use the value from the first capturing group.
-    //   If there is no first capturing group, that is an error! That can't be.
-    // - If old name is default, then we shouldn't also specify the actual name.
-    // - If new name is userDef, then we shoudln't specify the actual name.
-
-    // Returns a string in case when old import path is regex and it matched the given import path.
-    // Then, returned string is the value of the capturing group from the regex.
-    function tryMatchingAnImportPathOldImportPath(importPath: string): boolean | string {
-      const validSuffixes = ["", ".js", ".jsx", ".ts", ".tsx", "/index.js", "/index.ts"];
-      if (importMapping.old.path instanceof RegExp) {
-        const validPathRegexes = [];
-        for (const suffix of validSuffixes) {
-          validPathRegexes.push(new RegExp(importMapping.old.path.source + escapeRegExp(suffix)));
-        }
-        const matchingPathRegex = validPathRegexes.find((regex) => regex.test(importPath));
-        if (matchingPathRegex) {
-          const match = importPath.match(matchingPathRegex);
-          if (match) return match[1];
-        }
-        return false;
-      } else {
-        const validPaths = validSuffixes.map(
-          (suffix) => importMapping.old.path + suffix,
-        );
-        return validPaths.includes(importPath);
-      }
-    }
-
-    root
+    astRoot
       .find(j.ImportDeclaration)
       .forEach((astPath) => {
+        // Check if the import path matches the old import path of the import mapping.
+        // If it doesn't, skip this import declaration.
         const importPathsMatch = typeof astPath.value.source.value == "string" &&
-          tryMatchingAnImportPathOldImportPath(astPath.value.source.value)
+          tryMatchingImportPathToImportMappingOldPath(astPath.value.source.value, importMapping)
         if (!importPathsMatch) {
           return;
         }
+
+        // User defined part of the import path, if it matched on a regex.
+        // Example: if import path is `@wasp/actions/<someAction>`, this will be `someAction`.
+        // Example: if import path is `@wasp/config`, this will be `null`.
         const userDefImportPathName =
           typeof importPathsMatch === "string" ? importPathsMatch : null;
 
-        const doesOldImportNameMatchAnything: boolean =
-          !userDefImportPathName && importMapping.old.name === userDefName;
-
+        // Names (specifiers) that observed import declaration imports.
         const importSpecifiers = astPath.value.specifiers;
         if (!importSpecifiers) return;
 
@@ -188,34 +132,74 @@ export default function transformer(fileInfo: FileInfo, api: API, options: Optio
   }
 
   // Add new import declarations, with all the new specifiers we collected in `newImports`.
-  root.find(j.Program).forEach((path) => {
+  astRoot.find(j.Program).forEach((path) => {
     newImports.forEach((specifiers, source) => {
       path.node.body.unshift(j.importDeclaration(specifiers, j.literal(source)));
     });
   });
 
-  return root.toSource();
+  return astRoot.toSource();
 }
 
-function escapeRegExp(x: string) {
+// Returns a string in case when old import path is regex and it matched the given import path.
+// Then, returned string is the value of the capturing group from the regex.
+// Tries to match the given import path to the old import path of the given import mapping,
+// in the sense that it can and should be rewritten using the import mapping.
+//
+// Returns false if there is no match. true if there is a match, and string if there is a match
+// If there is a match and the old import path is a regex, it returns a string
+// which is the value of the first capturing group from the regex.
+// Otherwise, if it is a match but old import path is not a regex, it returns just true.
+function tryMatchingImportPathToImportMappingOldPath(
+  importPath: string, importMapping: ImportMapping
+): boolean | string {
+  const validSuffixes = ["", ".js", ".jsx", ".ts", ".tsx", "/index.js", "/index.ts"];
+  if (importMapping.old.path instanceof RegExp) {
+    const validPathRegexes = [];
+    for (const suffix of validSuffixes) {
+      validPathRegexes.push(new RegExp(importMapping.old.path.source + escapeRegExp(suffix)));
+    }
+    const matchingPathRegex = validPathRegexes.find((regex) => regex.test(importPath));
+    if (matchingPathRegex) {
+      const match = importPath.match(matchingPathRegex);
+      if (match) return match[1];
+    }
+    return false;
+  } else {
+    const validPaths = validSuffixes.map(
+      (suffix) => importMapping.old.path + suffix,
+    );
+    return validPaths.includes(importPath);
+  }
+}
+
+// Turns a string into a regex that matches that string exactly (escaping any special chars).
+function escapeRegExp(x: string): string {
   // Copied from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_expressions .
   return x.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
 }
 
-const defaultName = Symbol("default");  // Default import name, with some value.
-const userDefName = Symbol("userDefName");  // Non-default import name that is user defined.
+// Used to mark the import name as a default import.
+const defaultName = Symbol("default");
+// Used to mark the import name as a non-default import name that is user defined.
+const userDefName = Symbol("userDefName");
 
 // Old import name can be:
 // - string: named import of known, fixed name, e.g. `config` in `{ config } from '@wasp/config';`
 // - defaultName: default import, any name, e.g. `config` in `import config from '@wasp/config';`
 // - userDefName: named import whose name is defined by user,
-//     e.g. `myAction` in `{ myAction } from '@wasp/actions/myAction';`
-type OldImportName = string | typeof defaultName | typeof userDefName;
-// New import name is same type as old import name, but it can't be default name.
+//     e.g. `myAction` in `{ someAction } from '@wasp/actions/someAction';`
+type OldImportName = string | typeof userDefName | typeof defaultName;
+// New import name is of almost the same type as old import name, except it can't be default name.
 type NewImportName = string | typeof userDefName;
 
 interface ImportMapping {
   old: {
+    // NOTE: Old paths must be without any extensions like .js(x)/.ts(x)
+    // or /index.js or /index.ts suffixes.
+    // NOTE: All regexes are expected to have a single capturing group that
+    // captures the user defined part of the import path. Don't use regex
+    // if there is no user defined part of the import path.
     path: string | RegExp;
     name: OldImportName;
   };
@@ -226,14 +210,13 @@ interface ImportMapping {
   };
 }
 
-// NOTE: Old paths must be without any extensions or /index.js or /index.ts suffixes.
 const importMappings: ImportMapping[] = [
   {
-    old: { path: '@wasp/config.js', name: defaultName },
+    old: { path: '@wasp/config', name: defaultName },
     new: { path: 'wasp/server', name: 'config' }
   },
   {
-    old: { path: '@wasp/dbClient.js', name: defaultName },
+    old: { path: '@wasp/dbClient', name: defaultName },
     new: { path: 'wasp/server', name: 'prisma' }
   },
   {
@@ -250,11 +233,7 @@ const importMappings: ImportMapping[] = [
   },
   {
     old: { path: '@wasp/actions', name: 'OptimisticUpdateDefinition' },
-    new: {
-      path: 'wasp/client/operations',
-      name: 'OptimisticUpdateDefinition',
-      isType: true
-    }
+    new: { path: 'wasp/client/operations', name: 'OptimisticUpdateDefinition', isType: true }
   },
   {
     old: { path: /@wasp\/actions\/(\w+)/, name: defaultName },
@@ -325,40 +304,23 @@ const importMappings: ImportMapping[] = [
     new: { path: 'wasp/client/auth', name: 'signup' }
   },
   {
-    old: {
-      path: '@wasp/auth/providers/email/utils.js',
-      name: 'createEmailVerificationLink'
-    },
+    old: { path: '@wasp/auth/providers/email/utils', name: 'createEmailVerificationLink' },
     new: { path: 'wasp/server/auth', name: 'createEmailVerificationLink' }
   },
   {
-    old: {
-      path: '@wasp/auth/providers/email/utils.js',
-      name: 'sendEmailVerificationEmail'
-    },
+    old: { path: '@wasp/auth/providers/email/utils', name: 'sendEmailVerificationEmail' },
     new: { path: 'wasp/server/auth', name: 'sendEmailVerificationEmail' }
   },
   {
     old: { path: '@wasp/types', name: 'GetVerificationEmailContentFn' },
-    new: {
-      path: 'wasp/server/auth',
-      name: 'GetVerificationEmailContentFn',
-      isType: true
-    }
+    new: { path: 'wasp/server/auth', name: 'GetVerificationEmailContentFn', isType: true }
   },
   {
     old: { path: '@wasp/types', name: 'GetPasswordResetEmailContentFn' },
-    new: {
-      path: 'wasp/server/auth',
-      name: 'GetPasswordResetEmailContentFn',
-      isType: true
-    }
+    new: { path: 'wasp/server/auth', name: 'GetPasswordResetEmailContentFn', isType: true }
   },
   {
-    old: {
-      path: '@wasp/auth/forms/ForgotPassword',
-      name: 'ForgotPasswordForm'
-    },
+    old: { path: '@wasp/auth/forms/ForgotPassword', name: 'ForgotPasswordForm' },
     new: { path: 'wasp/client/auth', name: 'ForgotPasswordForm' }
   },
   {
@@ -366,10 +328,7 @@ const importMappings: ImportMapping[] = [
     new: { path: 'wasp/client/auth', name: 'LoginForm' }
   },
   {
-    old: {
-      path: '@wasp/auth/forms/ResetPassword',
-      name: 'ResetPasswordForm'
-    },
+    old: { path: '@wasp/auth/forms/ResetPassword', name: 'ResetPasswordForm' },
     new: { path: 'wasp/client/auth', name: 'ResetPasswordForm' }
   },
   {
@@ -382,11 +341,7 @@ const importMappings: ImportMapping[] = [
   },
   {
     old: { path: '@wasp/auth/forms/types', name: 'CustomizationOptions' },
-    new: {
-      path: 'wasp/client/auth',
-      name: 'CustomizationOptions',
-      isType: true
-    }
+    new: { path: 'wasp/client/auth', name: 'CustomizationOptions', isType: true }
   },
   {
     old: { path: '@wasp/auth/helpers/GitHub', name: 'SignInButton' },
@@ -405,21 +360,15 @@ const importMappings: ImportMapping[] = [
     new: { path: 'wasp/client/auth', name: 'googleSignInUrl' }
   },
   {
-    old: {
-      path: '@wasp/core/AuthError',
-      name: defaultName,
-    },
+    old: { path: '@wasp/core/AuthError', name: defaultName },
     new: { path: 'wasp/server', name: 'AuthError' }
   },
   {
-    old: {
-      path: '@wasp/core/HttpError',
-      name: defaultName,
-    },
+    old: { path: '@wasp/core/HttpError', name: defaultName },
     new: { path: 'wasp/server', name: 'HttpError' }
   },
   {
-    old: { path: '@wasp/dbSeed/types.js', name: 'DbSeedFn' },
+    old: { path: '@wasp/dbSeed/types', name: 'DbSeedFn' },
     new: { path: 'wasp/server', name: 'DbSeedFn', isType: true }
   },
   {
@@ -468,19 +417,11 @@ const importMappings: ImportMapping[] = [
   },
   {
     old: { path: '@wasp/webSocket', name: 'WebSocketDefinition' },
-    new: {
-      path: 'wasp/server/webSocket',
-      name: 'WebSocketDefinition',
-      isType: true
-    }
+    new: { path: 'wasp/server/webSocket', name: 'WebSocketDefinition', isType: true }
   },
   {
     old: { path: '@wasp/webSocket', name: 'WaspSocketData' },
-    new: {
-      path: 'wasp/server/webSocket',
-      name: 'WaspSocketData',
-      isType: true
-    }
+    new: { path: 'wasp/server/webSocket', name: 'WaspSocketData', isType: true }
   },
   {
     old: { path: '@wasp/auth', name: 'defineUserSignupFields' },
@@ -507,64 +448,43 @@ const importMappings: ImportMapping[] = [
     new: { path: 'wasp/auth', name: 'AuthUser', isType: true }
   },
   {
-    old: {
-      path: '@wasp/auth/validation.js',
-      name: 'ensurePasswordIsPresent'
-    },
+    old: { path: '@wasp/auth/validation', name: 'ensurePasswordIsPresent' },
     new: { path: 'wasp/server/auth', name: 'ensurePasswordIsPresent' }
   },
   {
-    old: { path: '@wasp/auth/validation.js', name: 'ensureValidPassword' },
+    old: { path: '@wasp/auth/validation', name: 'ensureValidPassword' },
     new: { path: 'wasp/server/auth', name: 'ensureValidPassword' }
   },
   {
-    old: { path: '@wasp/auth/validation.js', name: 'ensureValidEmail' },
+    old: { path: '@wasp/auth/validation', name: 'ensureValidEmail' },
     new: { path: 'wasp/server/auth', name: 'ensureValidEmail' }
   },
   {
-    old: { path: '@wasp/auth/validation.js', name: 'ensureValidUsername' },
+    old: { path: '@wasp/auth/validation', name: 'ensureValidUsername' },
     new: { path: 'wasp/server/auth', name: 'ensureValidUsername' }
   },
   {
-    old: { path: '@wasp/auth/utils.js', name: 'createProviderId' },
+    old: { path: '@wasp/auth/utils', name: 'createProviderId' },
     new: { path: 'wasp/server/auth', name: 'createProviderId' }
   },
   {
-    old: {
-      path: '@wasp/auth/utils.js',
-      name: 'sanitizeAndSerializeProviderData'
-    },
-    new: {
-      path: 'wasp/server/auth',
-      name: 'sanitizeAndSerializeProviderData'
-    }
+    old: { path: '@wasp/auth/utils', name: 'sanitizeAndSerializeProviderData' },
+    new: { path: 'wasp/server/auth', name: 'sanitizeAndSerializeProviderData' }
   },
   {
-    old: {
-      path: '@wasp/auth/utils.js',
-      name: 'updateAuthIdentityProviderData'
-    },
-    new: {
-      path: 'wasp/server/auth',
-      name: 'updateAuthIdentityProviderData'
-    }
+    old: { path: '@wasp/auth/utils', name: 'updateAuthIdentityProviderData' },
+    new: { path: 'wasp/server/auth', name: 'updateAuthIdentityProviderData' }
   },
   {
-    old: {
-      path: '@wasp/auth/utils.js',
-      name: 'deserializeAndSanitizeProviderData'
-    },
-    new: {
-      path: 'wasp/server/auth',
-      name: 'deserializeAndSanitizeProviderData'
-    }
+    old: { path: '@wasp/auth/utils', name: 'deserializeAndSanitizeProviderData' },
+    new: { path: 'wasp/server/auth', name: 'deserializeAndSanitizeProviderData' }
   },
   {
-    old: { path: '@wasp/auth/utils.js', name: 'findAuthIdentity' },
+    old: { path: '@wasp/auth/utils', name: 'findAuthIdentity' },
     new: { path: 'wasp/server/auth', name: 'findAuthIdentity' }
   },
   {
-    old: { path: '@wasp/auth/utils.js', name: 'createUser' },
+    old: { path: '@wasp/auth/utils', name: 'createUser' },
     new: { path: 'wasp/server/auth', name: 'createUser' }
   },
   {
